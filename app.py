1437bb4cd5d3e948192e21aa5b0eff76e7d046b5
Marcos8060 <marcosgav80@gmail.com>
2022-01-21 17:21:40 +0300

    checking passwords match

--- app.py
+++ app.py
@@ -3,4 +3,24 @@ from user import User
 
 def main():
     while True:
+        print("Welcome to Marcos Password locker!!")
+        print("Kindly select a short code to navigate: select 'c' to create a new account, 'l' to login and 'x' to logout from the application")
+        short_code = input().lower()
+        print('\n')
+
+        if short_code == 'c':
+# create username
+            print('create a username')
+            new_username = input()
+# create password
+            print('create a password')
+            new_password = input()
+# confirm password
+            print('confirm password')
+            confirm_password = input()
+# check passwords match
+            while new_password != confirm_password:
+                print('passwords do not match')
+
+
         
