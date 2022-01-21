2da64576610c009f7e20625698c3cc9294e2299d
Marcos8060 <marcosgav80@gmail.com>
2022-01-21 16:59:12 +0300

    created the blueprints for my user object

--- user.py
+++ user.py
@@ -1,3 +1,9 @@
 class User:
-    def __init__(self,name):
-        self.name = name
+    def __init__(self,user_name,password):
+        self.user_name = user_name
+        self.password = password
+
+user_credentials =[]
+
+def save_Credentials(self):
+    self.user_credentials.append()
