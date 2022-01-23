2c96019d7df7a82383678663bd6ca85e3944475a
Marcos8060 <marcosgav80@gmail.com>
2022-01-23 08:18:32 +0300

    created a method to save a user in the contact list

--- user.py
+++ user.py
@@ -1,1 +1,20 @@
+class User:
+    contact_list = []
+    def __init__(self,user_name,password):
+        self.user_name = user_name
+        self.password = password
+
+        # save user in the contact_list
+    def save_user(self):
+        User.contact_list.append(self)
+
+    # delete user from the contact_list
+    def delete_user(self):
+        User.contact_list.remove(self)
+
+    # display users from the contact_list
+    @classmethod
+    def display_user(cls):
+        return cls.contact_list
+        
 
