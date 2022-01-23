fafd99fb3669ee11a3e722e0c26fe3834fc39038
Marcos8060 <marcosgav80@gmail.com>
2022-01-23 08:47:09 +0300

    made a second test for the save user functionaliry

--- test.py
+++ test.py
@@ -15,6 +15,12 @@ class TestUser(unittest.TestCase):
     def test_init(self):
         self.assertEqual(self.new_User.user_name,'marcos')
         self.assertEqual(self.new_User.password,'1234567')
+    
+    # method to test saving a user
+    def test_save_user(self):
+
+        self.new_User.save_user()
+        self.assertEqual(len(User.contact_list),1)
 
 if __name__ == '__main__':
     unittest.main()
