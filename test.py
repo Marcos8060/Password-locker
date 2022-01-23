41961a8874a40bfd3922e3b1a4c330b00c54a340
Marcos8060 <marcosgav80@gmail.com>
2022-01-23 08:42:38 +0300

    ran a first test to confirm that the object is instantiated correctly

--- test.py
+++ test.py
@@ -6,3 +6,15 @@ class TestUser(unittest.TestCase):
     # cleanup method after every test case
     def tearDown(self):
         User.contact_list = []
+
+    # method that runs before each test case
+    def setUp(self):
+        self.new_User = User('marcos','1234567')
+
+    # method to confirm that the object has been instantiated correctly
+    def test_init(self):
+        self.assertEqual(self.new_User.user_name,'marcos')
+        self.assertEqual(self.new_User.password,'1234567')
+
+if __name__ == '__main__':
+    unittest.main()
