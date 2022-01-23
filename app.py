38528665e3a5b57f1a029314e3d5400cd052f6bf
Marcos8060 <marcosgav80@gmail.com>
2022-01-23 13:09:13 +0300

    made a minor change in the error message

--- app.py
+++ app.py
@@ -118,85 +118,11 @@ def main():
         elif short_code == 'x':
             print('Thank you for visiting password locker')
             break
+        else:
+            print('Enter a valid short code to continue.')
 
 if __name__ == '__main__':
     main()
-
-
-
-# from json.tool import main
-# from user import User
-
-# def main():
-#     while True:
-#         print("Welcome to Password locker!!")
-#         print("Kindly select a short code to navigate: select 'c' to create a new account, 'l' to login and 'x' to logout from the application")
-#         short_code = input().lower()
-#         print('\n')
-
-#         if short_code == 'c':
-
-#             print('create a username')
-#             new_username = input()
-
-#             print('create a password')
-#             new_password = input()
-
-#             print('confirm password')
-#             confirm_password = input()
-
-#             while new_password != confirm_password:
-
-#                 print('passwords do not match!!')
-#                 print('create new password')
-#                 new_password = input()
-#                 print('confirm password')
-#                 confirm_password = input()
-#             else:
-
-#                 print(f'Congratulations {new_username} your account is created successfully')
-#                 print('\n')
-
-#                 print('proceed to login')
-#                 print('Enter username')
-#                 login_username = input()
-#                 print('Enter password')
-#                 login_password = input()
-
-#                 while login_username != new_username or login_password != new_password:
-#                     print('Sorry you entered wrong cedentials')
-#                     print('Try loging in again')
-#                     login_username = input()
-#                     print('Enter password')
-#                     login_password = input()
-
-#                 else:
-#                     print(f'congaratulations {login_username}, your login is successful')
-#         elif short_code == 'l':
-#             print('Enter username')
-#             default_username = input()
-#             print('Enter password')
-#             default_password = input()
-
-#             while default_username != 'hello world' or default_password != '12345':
-#                 print('Invalid credentials, you username is "hello world" and password is "12345"')
-#                 print("Enter username")
-#                 default_username = input()
-#                 print('Enter password')
-#                 default_password = input()
-#             else:
-#                 print(f'Congratulations {default_username}, welcome to password locker')
-#                 print('/n')
-        
-#         elif short_code == 'x':
-#             break
-#         else:
-#             print('Enter a valid code to continue')
-
-# if __name__ == '__main__':
-#     main()
-
-                
 
 
 
