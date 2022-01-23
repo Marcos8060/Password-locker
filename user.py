7906fb45a36576ead4b910371825ee9d0682df45
Marcos8060 <marcosgav80@gmail.com>
2022-01-23 08:04:59 +0300

    created the contact file

--- user.py
+++ user.py
@@ -1,9 +1,1 @@
-class User:
-    user_credentials =[]
-    def __init__(self,user_name,password):
-        self.user_name = user_name
-        self.password = password
 
-
-def save_Credentials(self):
-    User.user_credentials.append(self)
