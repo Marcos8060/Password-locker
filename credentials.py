5e52fa7ac506d291aa1dacb3f5ab0958ad22e8c3
Marcos8060 <marcosgav80@gmail.com>
2022-01-23 08:24:43 +0300

    created a credentials class

--- credentials.py
+++ credentials.py
@@ -1,1 +1,8 @@
-print( '😋')
+class Credentials:
+    # declaring an empty user accounts list
+    user_accounts = []
+    def __init__(self,user_name,user_password):
+        self.user_name = user_name
+        self.user_password = user_password
+
+    # 
